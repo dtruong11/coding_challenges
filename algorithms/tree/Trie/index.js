f51601b2e1a33d872e806bd4fe7a5969f2be59d4
Diep Truong <29584874+dtruong11@users.noreply.github.com>
2023-06-07 12:13:53 -0700

    Update Trie Implementation

--- algorithms/tree/Trie/index.js
+++ algorithms/tree/Trie/index.js
@@ -3,68 +3,143 @@ https://leetcode.com/problems/implement-trie-prefix-tree/discuss/613676/JavaScri
 https://leetcode.com/problems/implement-trie-prefix-tree/discuss/422168/JavaScript-Solution
 https://github.com/mission-peace/interview/blob/master/src/com/interview/suffixprefix/Trie.java
 */
+/*
+WHAT I STRUGGLE WITH
+- how to handle multiple children. 
+*/
+
+/** 
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
 
 class TrieNode {
-	constructor() {
-		this.children = {}
-		this.isEndOfWord = false
-	}
+    constructor(char) {
+        this.val = char
+        this.children = {}
+        this.isEnd = false
+    }
 }
 
 class Trie {
-	constructor() {
-		this.root = new TrieNode()
-	}
+    constructor() {
+        this.node = new TrieNode()
+    }
 
-	/**
-	 * Inserts a word into the trie. 
-	 * @param {string} word
-	 * @return {void}
-	 */
-	insert(word) {
-		let current = this.root
-		for (let i = 0; i < word.length; i++) {
-			let childNode = current.children[word[i]] || new TrieNode(word[i])
-			current.children[word[i]] = childNode
-			current = childNode
-		}
-		current.isEndOfWord = true
-		return this
-	}
+    /**
+    INSERT
+    - get the current root node
+    - loop through the word
+    - get current node's children => check any similar child exists. 
+        - not exist => create & attach the new TrieNode
+        - exist => reuse 
+    - set current to newly created childNode
+    - set current to TRUE => mark word end
 
-	/**
-	 * Returns if the word is in the trie. 
-	 * @param {string} word
-	 * @return {boolean}
-	 */
-	search(word) {
-		let current = this.root
-		for (let i = 0; i < word.length; i++) {
-			current = current.children[word[i]]
-			if (!current) return false
-		}
-		return current.isEndOfWord
-	}
+    Time: O(m) - m length of key. Either check or create up to m
+    Space: O(m) - tree will have a length of O(m)
+     */
+    insert(word) {
+        let curNode = this.node
+        for (let i = 0; i < word.length; i++) {
+            const char = word[i]
+            if (!curNode.children[char]) {
+                curNode.children[char] = new TrieNode(char)
+            }
+            curNode = curNode.children[char]
+        }
+        curNode.isEnd = true
+        return this
+    }
 
-	/**
-	 * Returns if there is any word in the trie that starts with the given prefix. 
-	 * @param {string} prefix
-	 * @return {boolean}
-	*/
-	startsWith(prefix) {
-		let current = this.root
-		for (let i = 0; i < prefix.length; i++) {
-			current = current.children[prefix[i]]
-			if (!current) return false
-		}
-		return true
-	}
+    /**
+    SEARCH
+    - get the current root node
+    - loop through word character
+        - if not found word char in current's children => FALSE
+        - set current to that matching child node to go down the tree 
+    - return true if at the end of word
 
-	remove(word) {
+    Time: O(m)
+    Space: O(1)
+     */
+    search(word) {
+        let curNode = this.node
+        for (let i = 0; i < word.length; i++) {
+            const char = word[i]
+            if (!curNode.children[char]) {
+                return false
+            }
+            curNode = curNode.children[char]
+        }
+        return curNode.isEnd
 
-	}
-}
+    }
 
+    /**
+    STARTSWITH - Prefix Check
+    - similar to search. Don't have to check word end 
+    Time: O(m)
+    Space: O(1)
+     */
+    startsWith(word) {
+        let curNode = this.node
+        for (let i = 0; i < word.length; i++) {
+            const char = word[i]
+            if (!curNode.children[char]) {
+                return false
+            }
+            curNode = curNode.children[char]
+        }
+        return true
+    }
 
-const dict = new Trie()
-dict.insert('hello')
+    /**
+    DELETE WORD
+    - Use Recursion 
+    - High level: Go down the trie, once the whole word is found and at the end of the word. 
+        - only do more if isEnd: true. skip further action if isEnd: false
+            - flip isEnd: false
+            - if the word is NOT a prefix to any longer word (no children for the last character) 
+                -> should delete that character out of its parent map. Return true recursively upward to remove that char from the children map of its parent. 
+            - if the word is a prefix to any longer word, don't remove it from its parent map. 
+     */
+
+    _validateCharExist(node, character) {
+        const charNode = node.children[character]
+        if (!charNode) return false // Word character not found, no need to delete
+        return true
+    }
+
+    _handleEndOfWord(node) {
+        if (!node.isEnd) {
+            return false; // Word not found, no need to delete
+        }
+        node.isEnd = false
+        return Object.keys(node.children).length === 0; // Can delete because node has no child
+    }
+
+    _shouldDeleteChar(node, idx, word) {
+        // base case - traverse down to the end of the word
+        if (idx === word.length) {
+            return this._handleEndOfWord(node)
+        }
+
+        const character = word[idx]
+        if (!this._validateCharExist(node, character)) return false
+
+        const shouldDeleteCurNode = this._shouldDeleteChar(node, idx + 1, word)
+        if (shouldDeleteCurNode) {
+            delete node.children[character] // delete this char from its parent mapping
+            return Object.keys(node.children).length === 0 // return true for its parent to be deleted as well, if parent has no child
+        }
+        return false
+    }
+
+    delete(word) {
+        return this._shouldDeleteChar(this.root, 0, word)
+    }
+}dict.insert('hello')
